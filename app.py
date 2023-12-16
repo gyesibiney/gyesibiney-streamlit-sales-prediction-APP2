a233b73e9ba06d2f20f127832baf68395f4ba612
David Gyesi BINEY <gyesibiney@users.noreply.huggingface.co>
2023-12-16 22:26:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ st.markdown('this predict sales')
 # data loading
 model= joblib.load('saved_ml.joblib')
 
-
 # inputs
 st.header('make a forecast here:')
 ds= st.date_input(label='Please enter your forecast date')
